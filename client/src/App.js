06f0d60b42e20ab3a47764b2a082516b4de90a17
Sawinu Chandira Peiris <savinu.chandira2002@gmail.com>
2023-08-29 23:29:54 +0530

    get a start for profile page

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,20 @@ import PopularTrip from './components/populartrip/PopularTrip';
 import Whatwedo from './components/whatWeDo/Whatwedo';
 import ContactUs from './components/contactus/ContactUs';
 import Footer from './components/footer/Footer';
+import Profileform from './components/profilepage/Profileform';
 
 function App() {
   return (
     <div>
       <NavBar/>
-      <HeroSection/>
+
+      {/* <HeroSection/>
       <About/>
       <PopularTrip/>
       <ContactUs/>
-      <Whatwedo/>
+      <Whatwedo/> */}
+
+      <Profileform/>
       <Footer/>
     </div>
   );
---
+++ client/src/components/profilepage/Profileform.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import "./Profileform.css"
+import userlogo from "../../assets/facebook 1.png"
+
+const Profileform = () => {
+  return (
+    <div>
+        <div className='head'>
+            <img src={userlogo} alt=''/>
+            <h2>PROFILE</h2>
+        </div>
+        <div className='formpart'>
+            <form>
+                <div className='subform'>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Name</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter your Name"/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Age</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter your age"/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Gender</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Gender"/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Enter your travel type</label>
+                        <select class="form-select" aria-label="Default select example">
+                            <option value="1">Travel type 1</option>
+                            <option value="2">Travel type 2</option>
+                            <option value="3">Travel type 3</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className='subform'>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Adventures</label>
+                        <select class="form-select" aria-label="Default select example">
+                            <option value="1">Adventures 1</option>
+                            <option value="2">Adventures 2</option>
+                            <option value="3">Adventures 3</option>
+                        </select>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Disability</label>
+                        <textarea class="form-control" placeholder="Enter your Disability"></textarea>
+                    </div>
+
+                </div>
+                <div className='subform'>wfew</div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Profileform
