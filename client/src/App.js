1265a70d4d6af7fef0bc5c579cbd9b422275ed7b
Sawinu Chandira Peiris <savinu.chandira2002@gmail.com>
2023-08-29 20:46:14 +0530

    updated2

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,11 @@
 import './App.css';
 import About from './components/aboutus/About';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import HeroSection from './components/heroSection/HeroSection';
 import NavBar from './components/navBar/NavBar';
 import PopularTrip from './components/populartrip/PopularTrip';
 import Whatwedo from './components/whatWeDo/Whatwedo';
 import ContactUs from './components/contactus/ContactUs';
 import Footer from './components/footer/Footer';
-=======
-=======
->>>>>>> 928c7f9a8a9103d85c5461fb15007bb0aeb74790
-
-import ContactUs from './components/contactus/ContactUs';
-import NavBar from './components/navBar/NavBar';
-import PopularTrip from './components/populartrip/PopularTrip';
-import HeroSection from './components/heroSection/HeroSection';
-import Whatwedo from './components/whatWeDo/Whatwedo';
-
-<<<<<<< HEAD
->>>>>>> 928c7f9a8a9103d85c5461fb15007bb0aeb74790
-=======
->>>>>>> 928c7f9a8a9103d85c5461fb15007bb0aeb74790
 
 function App() {
   return (
@@ -32,13 +16,7 @@ function App() {
       <PopularTrip/>
       <ContactUs/>
       <Whatwedo/>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <Footer/>
-=======
->>>>>>> 928c7f9a8a9103d85c5461fb15007bb0aeb74790
-=======
->>>>>>> 928c7f9a8a9103d85c5461fb15007bb0aeb74790
     </div>
   );
 }
