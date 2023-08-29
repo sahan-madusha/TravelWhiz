b76cc2d282163c5bbb68e413d4e5efdfe0439746
tracialk <traciaplan@gmail.com>
2023-08-29 15:40:42 +0530

    what we do section

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/aboutus/About';
 import HeroSection from './components/heroSection/HeroSection';
 import NavBar from './components/navBar/NavBar';
+import Whatwedo from './components/whatWeDo/Whatwedo';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <NavBar/>
       <HeroSection/>
       <About/>
-      
+      <Whatwedo/>
     </div>
   );
 }
