e38a867b60d1beed4e2b97f20a44050e7f1aa8ea
tracialk <traciaplan@gmail.com>
2023-08-29 15:03:55 +0530

    hero section and about-Us section.

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -4,7 +4,7 @@ import './NavBar.css'
 const NavBar = () => {
     return (
 
-        <nav class="navbar navbar-expand-lg bg-transparent fixed-top navToggle">
+        <nav class="navbar navbar-expand-lg navColor fixed-top  navToggle">
             <div class="container-fluid mx-5">
                 <a class="navbar-brand col-lg-1 fw-bold" href="#">TravelWhiz.lk</a>
 
@@ -12,11 +12,11 @@ const NavBar = () => {
                     <i class="bi bi-list text-dark fs-1"></i>
                 </button>
 
-                <div class="collapse navbar-collapse col-lg-10 justify-content-lg-end justify-content-start  align-items-center" id="navbarNavAltMarkup">
+                <div class="collapse navbar-collapse col-lg-10 justify-content-lg-end justify-content-start   align-items-center" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link navbar-link me-4" href="#">Home</a>
-                        <a class="nav-link navbar-link me-4" href="#">About us</a>
-                        <a class="nav-link navbar-link me-4" href="#">Contact us</a>
+                        <a class="nav-link text-dark fw-bold navbar-link me-4" href="#">Home</a>
+                        <a class="nav-link text-dark fw-bold navbar-link me-4" href="#">About us</a>
+                        <a class="nav-link text-dark fw-bold navbar-link me-4" href="#">Contact us</a>
                         <a class="btn btn-sm border border-1 border-dark px-auto bg-dark text-light rounded-pill text-center nav-link btn-lg-width btn-sm-width " href="#"><i class="bi bi-person-fill"></i> Profile</a>               
                     </div>
                 </div>
